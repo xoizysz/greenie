e8b3b309bedf3cfe9df5d1fa591276454c65c61f
Paramita Debbarma <paramitadebbarma404@gmail.com>
2025-06-20 20:48:12 +0530

    final

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceHubEmbeddings
+
 
 def load_pdf_file(path):
     loader = DirectoryLoader(path, glob="*.pdf", loader_cls=PyPDFLoader)
@@ -18,7 +19,11 @@ def text_split(documents):
     return splitter.split_documents(documents)
 
 def download_hugging_face_embeddings():
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return HuggingFaceHubEmbeddings(
+      repo_id="sentence-transformers/all-MiniLM-L6-v2",
+      huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    )
+
 
 
 # ─── System Prompt (inlined) ─────────────────────────────────────────────────
